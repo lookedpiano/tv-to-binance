d42236d3df2e5c8fd404b70a2545a881e1d7862f
jimmy friedrich <jimmyfriedrich@jimmys-macbook-pro.home>
2025-07-23 09:01:43 +0200

    timestamps in milliseconds

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def get_current_price(symbol):
     return price
 
 def get_timestamp():
-    return int(requests.get("https://api.binance.com/api/v3/time").json()["serverTime"] / 1000)
+    return int(requests.get("https://api.binance.com/api/v3/time").json()["serverTime"])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
