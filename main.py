a95c53c11226e76519d140a4a211a038a692a3dc
jimmy friedrich <jimmyfriedrich@jimmys-macbook-pro-2.home>
2025-08-05 20:52:33 +0200

    deploy commit

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ logging.basicConfig(
 
 app = Flask(__name__)
 
-
 # Load from environment variables
 BINANCE_API_KEY = os.environ.get("BINANCE_API_KEY")
 BINANCE_SECRET_KEY = os.environ.get("BINANCE_SECRET_KEY")
